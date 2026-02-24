5872dd22b03ffe887df33eb045a7a843f834dff1
Usaid Ahmad <usaidmoiza@gmail.com>
2026-02-24 12:10:19 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(), react()],
+  base:'/usaid9.github.io/',
 
   server: { host: "localhost", port: 3000 },
 });
+
